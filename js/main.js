c39e81c183239aa0e9328af3bc7d9b3e042707a1
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2020-05-03 22:18:24 +0600

    Dynamic data now comes from db.json, gallery plugins added, design updated

--- js/main.js
+++ js/main.js
@@ -8,154 +8,47 @@
 - normalize.css
  */
 
+const DELAY_BETWEEN_ELEMENTS_LONG = 1000; // 1000
+const DELAY_BETWEEN_ELEMENTS_SHORT = 500; // 500
+const TYPE_SPEED = 16; // 16
+
+const objCompareFn = function (a, b) {
+    let A = a.name.toUpperCase();
+    let B = b.name.toUpperCase();
+    if (A < B) {
+        return -1;
+    }
+    if (A > B) {
+        return 1;
+    }
+    return 0;
+};
+
 $(function () {
 
-    {
+    $(".loading").fadeOut();
 
-        $(".loading").fadeOut();
+    $(".profile-img").addClass("ready");
 
-        $(".profile-img").addClass("ready");
+    new Typed(".profile-welcome .title", {
+        stringsElement: '.for-title',
+        typeSpeed: TYPE_SPEED,
+        startDelay: DELAY_BETWEEN_ELEMENTS_LONG,
+        backDelay: 1500,
+        showCursor: false,
+        loop: false,
+        onComplete: function (self) {
 
-        var DELAY_BETWEEN_ELEMENTS_LONG = 1000; //1000
-        var DELAY_BETWEEN_ELEMENTS_SHORT = 500; //500
-        var TYPE_SPEED = 16; // 25
-
-        var typedTitle = new Typed(".profile-welcome .title", {
-            stringsElement: '.for-title',
-            typeSpeed: TYPE_SPEED,
-            startDelay: DELAY_BETWEEN_ELEMENTS_LONG,
-            backDelay: 1500,
-            showCursor: false,
-            loop: false,
-            onComplete: function (self) {
-                showBio();
-            }
-        });
-
-        function showBio() {
-            var typedBio = new Typed(".profile-welcome .bio", {
-                stringsElement: '.for-bio',
-                typeSpeed: TYPE_SPEED,
-                startDelay: DELAY_BETWEEN_ELEMENTS_LONG,
-                showCursor: false,
-                loop: false,
-                onComplete: function (self) {
-                    setTimeout(function () {
-
-                        showDownButton();
-
-                    }, DELAY_BETWEEN_ELEMENTS_SHORT);
-                }
-            });
-        }
-
-        function showDownButton() {
-
-            var socialButtonPanel = $(".profile-welcome");
-            var socialIconBottom = socialButtonPanel.offset().top + socialButtonPanel.outerHeight();
-            var totalHeight = $("#one").height();
-
-            // Bottom Padding of #div-intro: 48
-            // Height of .show-more: 64
-            // Extra space for safety: 8px
-            var newMarginTop = totalHeight - (socialIconBottom + 48 + 64 + 8);
-
-            if (newMarginTop > 0) {
-                $(".show-more").css("margin-top", newMarginTop);
-            }
-
-            setTimeout(function () {
-                $(".show-more").fadeIn(function () {
-
-                    showTheRest();
-
-                });
-            }, DELAY_BETWEEN_ELEMENTS_SHORT);
+            showBio();
 
         }
-
-        function showTheRest() {
-            $(".the-rest").slideDown(function () {
-
-                drawParticles();
-
-                showNavBox();
-
-                sortLangSkills();
-
-                sortFrameworkSkills();
-
-            });
-        }
-
-        function drawParticles() {
-            particlesJS.load('particles-js', 'assets/particles.json');
-        }
-
-        function showNavBox() {
-            $("nav").css("display", "");
-        }
-
-        function sortLangSkills() {
-
-            var languages = [
-                "C",
-                "C++",
-                "Java",
-                "JavaScript",
-                "PHP",
-                "Python"
-            ];
-
-            languages.sort();
-
-            var langSkillParent = document.querySelector('#div-lang-skills .skill-tags .el-spans');
-
-            languages.forEach(function (value) {
-
-                var el_span = document.createElement('span');
-                el_span.innerText = value;
-
-                langSkillParent.append(el_span);
-
-            });
-
-        }
-
-        function sortFrameworkSkills() {
-
-            var frameworks = [
-                "Android",
-                "Django",
-                "Laravel",
-                "jQuery",
-                "Firestore Database",
-                "Firebase Realtime Database",
-                "Google Maps API"
-            ];
-
-            frameworks.sort();
-
-            var techSkillParent = document.querySelector('#div-tech-skills .skill-tags .el-spans');
-
-            frameworks.forEach(function (value) {
-
-                var el_span = document.createElement('span');
-                el_span.innerText = value;
-
-                techSkillParent.append(el_span);
-
-            });
-
-        }
-
-    }
+    });
 
     $("#go-to-more").click(function (event) {
         event.preventDefault();
 
         $('html, body').animate({
-            scrollTop: $(".study-timeline").offset().top
+            scrollTop: $("#div-bio").offset().top
         }, 1000);
 
     });
@@ -172,7 +65,7 @@ $(function () {
     $("a[data-msg='on']").click(function (event) {
         event.preventDefault();
 
-        var targetId = $(this).attr("data-target");
+        const targetId = $(this).attr("data-target");
 
         $("#" + targetId).fadeIn();
 
@@ -182,21 +75,12 @@ $(function () {
         $(this).fadeOut();
     });
 
-
     // Nav bar will hide when intro page is visible
-    // var $navEl = $("#nav");
-
-    // $(window).scrollspy({target: '#nav'});
-
-    // $(window).on('activate.bs.scrollspy', function () {
-    //     console.log("This event is not firing...");
-    // });
-
     $(window).on('activate.bs.scrollspy', function (e, obj) {
 
-        var spyedElId = $("#nav li > a.active").attr("href");
+        const $spyedElId = $("#nav li > a.active").attr("href");
 
-        if (spyedElId === "#one") {
+        if ($spyedElId === "#one") {
             $("#nav").removeClass("nav-show");
         } else {
             $("#nav").addClass("nav-show");
@@ -204,45 +88,245 @@ $(function () {
 
     });
 
-    initAchievements();
-
 });
 
-function initAchievements() {
-    var items = [];
+function showBio() {
+    new Typed(".profile-welcome .bio", {
+        stringsElement: '.for-bio',
+        typeSpeed: TYPE_SPEED,
+        startDelay: DELAY_BETWEEN_ELEMENTS_LONG,
+        showCursor: false,
+        loop: false,
+        onComplete: function (self) {
+            setTimeout(function () {
 
-    $.getJSON("json/db_achievements.json", function (data) {
+                showDownButton();
 
-        $.each(data, function (key, value) {
+            }, DELAY_BETWEEN_ELEMENTS_SHORT);
+        }
+    });
+}
 
-            item = '<div class="tl-el"> \
-<p class="tl-type">' + value.year + '</p> \
-<p class="tl-title">' + value.title + '</p> \
-<p class="tl-tags"><span>' + value.tags.join("</span><span>") + '</span></p>';
+function showDownButton() {
 
-            if (value.about !== false) {
-                item += '<p class="tl-about">' + value.about + '</p> \
-<hr class="tl-hr">';
-            }
+    const socialButtonPanel = $(".profile-welcome");
+    const socialIconBottom = socialButtonPanel.offset().top + socialButtonPanel.outerHeight();
+    const totalHeight = $("#one").height();
 
-            item += '<p class="tl-description">' + value.description + '</p>';
+    // Bottom Padding of #div-intro: 48
+    // Height of .show-more: 64
+    // Extra space for safety: 8px
+    const newMarginTop = totalHeight - (socialIconBottom + 48 + 64 + 8);
 
-            if (value.link !== false) {
-                item += '<div class="dl-box"> \
-<a class="btn-full-block" target="_blank" href="' + value.link + '">' + value.link_title + '</a> \
-</div>';
-            }
+    if (newMarginTop > 0) {
+        $(".show-more").css("margin-top", newMarginTop);
+    }
 
-            item += '</div>';
+    setTimeout(function () {
+        $(".show-more").fadeIn(function () {
 
-            items.push(item);
+            showTheRest();
 
         });
+    }, DELAY_BETWEEN_ELEMENTS_SHORT);
 
-        $("<div>", {
-            "class": "timeline",
-            html: items.join("")
-        }).appendTo("#div-projects");
+}
+
+function showTheRest() {
+    $(".the-rest").slideDown(function () {
+
+        drawParticles();
+
+        showNavBox();
+
+        initInformation();
 
     });
+}
+
+function drawParticles() {
+    particlesJS.load('particles-js', 'assets/particles.json');
+}
+
+function showNavBox() {
+    $("nav").css("display", "");
+}
+
+function initSiteInfo(info) {
+    const element = document.querySelector('#iw-site-info');
+
+    element.innerHTML = "Last updated on ― " + info.updated_at + " (v" + info.version + ")."
+}
+
+function sortLangSkills(languages) {
+
+    languages.sort(objCompareFn);
+
+    const langSkillParent = document.querySelector('#div-lang-skills .skill-tags .el-spans');
+
+    langSkillParent.innerHTML = ''
+
+    for (let i = 0; i < languages.length; i++) {
+
+        const obj = languages[i];
+
+        const elSpan = document.createElement('span');
+        elSpan.innerText = obj.name;
+
+        if (obj.active) {
+            elSpan.classList.add("active")
+            elSpan.setAttribute("data-toggle", "tooltip")
+            elSpan.setAttribute("title", "Recently Working On")
+        }
+
+        langSkillParent.append(elSpan);
+
+    }
+
+}
+
+function sortFrameworkSkills(frameworks) {
+
+    frameworks.sort(objCompareFn);
+
+    const techSkillParent = document.querySelector('#div-tech-skills .skill-tags .el-spans');
+
+    techSkillParent.innerHTML = ''
+
+    for (let i = 0; i < frameworks.length; i++) {
+
+        const obj = frameworks[i];
+
+        const elSpan = document.createElement('span');
+        elSpan.innerText = obj.name;
+
+        if (obj.active) {
+            elSpan.classList.add("active")
+            elSpan.setAttribute("data-toggle", "tooltip")
+            elSpan.setAttribute("title", "Recently Working On")
+        }
+
+        techSkillParent.append(elSpan);
+
+    }
+
+}
+
+function initAchievements(achievements) {
+
+    const timelineParent = document.querySelector('#timeline-parent');
+    timelineParent.innerHTML = ''
+
+    for (let i = 0; i < achievements.length; i++) {
+
+        const obj = achievements[i];
+
+        const tlEl = document.createElement("div");
+        tlEl.classList.add("tl-el")
+        timelineParent.append(tlEl)
+
+        const pTlType = document.createElement("p");
+        pTlType.classList.add("tl-type")
+        pTlType.innerHTML = obj.year
+        tlEl.append(pTlType)
+
+        const pTlTitle = document.createElement("p");
+        pTlTitle.classList.add("tl-title")
+        pTlTitle.innerHTML = obj.title
+        tlEl.append(pTlTitle)
+
+        const pTlTags = document.createElement("p");
+        pTlTags.classList.add("tl-tags")
+        pTlTags.innerHTML = '<span>' + obj.tags.join("</span><span>") + '</span>'
+        tlEl.append(pTlTags)
+
+        if (obj.about !== false) {
+            const pTlAbout = document.createElement("p");
+            pTlAbout.classList.add("tl-about")
+            pTlAbout.innerHTML = obj.about
+            tlEl.append(pTlAbout)
+
+            const pTlHr = document.createElement("hr");
+            pTlHr.classList.add("tl-hr")
+            tlEl.append(pTlHr)
+        }
+
+        const pTlDesc = document.createElement("p");
+        pTlDesc.classList.add("tl-description")
+        pTlDesc.innerHTML = obj.description
+        tlEl.append(pTlDesc)
+
+        if ("link" in obj || "previews" in obj) {
+
+            const btnDivContainer = document.createElement("div");
+            btnDivContainer.classList.add("dl-box")
+            tlEl.append(btnDivContainer)
+
+            if ("link" in obj) {
+                const btn = document.createElement("a");
+                btn.classList.add("btn-full-block")
+                btn.setAttribute("target", "_blank")
+                btn.setAttribute("href", obj.link)
+                btn.innerHTML = '<i class="fas fa-download"></i> ' + obj.link_title
+                btnDivContainer.append(btn);
+            }
+
+            if ("previews" in obj) {
+
+                let previewImageList = obj.previews;
+
+                const btn = document.createElement("button");
+                btn.classList.add("btn-full-block")
+                btn.innerHTML = '<i class="far fa-images"></i> ' + "Screenshots"
+                btnDivContainer.append(btn)
+
+                btn.addEventListener("click", function () {
+
+                    const pswpElement = document.querySelectorAll('.pswp')[0];
+                    const gallery = new PhotoSwipe(
+                        pswpElement,
+                        PhotoSwipeUI_Default,
+                        previewImageList,
+                        {
+                            shareEl: false,
+                            bgOpacity: 0.85
+                        }
+                    );
+                    gallery.init();
+
+                })
+            }
+
+        }
+
+    }
+
+}
+
+function initLongBio(longBio) {
+
+    const timelineParent = document.querySelector('#div-bio p');
+
+    timelineParent.innerHTML = longBio
+
+}
+
+function initInformation() {
+
+    $.getJSON("json/db.json?v=1", function (data) {
+
+        initSiteInfo(data.info);
+
+        initLongBio(data.long_bio)
+
+        sortLangSkills(data.languages);
+
+        sortFrameworkSkills(data.frameworks);
+
+        initAchievements(data.achievements);
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+    });
+
 }
